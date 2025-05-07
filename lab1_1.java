d81a526a6b41f7a7a63f09ca500f2c86a9ea3b3e
Duureeee <167115601+Duureeee@users.noreply.github.com>
2025-05-08 04:00:13 +0800

    lab1_1

---
+++ lab1_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class lab1_1 
+{public static void main(String[] args) {
+    // Бодлогын өгүүлбэр:
+    // Хэрэглэгчээс насны утга авч, тухайн хүнийг насанд хүрсэн эсэхийг шалга.
+    // Хэрвээ нас 18 болон түүнээс дээш байвал 'Насанд хүрсэн байна' гэж хэвлэ.
+    // Үгүй бол 'Насанд хүрээгүй байна' гэж хэвлэ.
+
+    // Scanner ашиглан хэрэглэгчээс өгөгдөл авна
+    Scanner scanner = new Scanner(System.in);
+
+    // Хэрэглэгчээс нас оруулахыг асуух
+    System.out.print("nasaa oruulna uu: ");
+    int nas = scanner.nextInt();
+
+    // Насны утга дээр үндэслэн насанд хүрсэн эсэхийг шалгана
+    if (nas >= 18) {
+        System.out.println("nasand hursen baina.");
+    } else {
+        System.out.println("nasand hureegui baina.");
+    }
+
+    // Scanner-ийг хаах
+    scanner.close();
+}
+}
